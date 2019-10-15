869bc38025fcf52c51e1bb0fadb2e406f57faea8
sbu470 <sbu470@edu.sollentuna.se>
2019-10-15 15:23:51 +0200

    Update

--- src/kapitel07_Villkorssatser/Grunder.java
+++ src/kapitel07_Villkorssatser/Grunder.java
@@ -4,8 +4,43 @@ import java.util.Scanner;
 
 public class Grunder {
 
+    private static boolean uttryck1;
+    private static boolean uttryck2;
+
     public static void main(String[] args) {
-        // TODO
+        ifSats();
+        ifElseSats();
+        ifElseIfElseSats();
+    }
+
+    private static void ifSats() {
+        // Syntax för if-sats:
+        if(uttryck1){
+            // Satser
+        }
+    }
+
+    private static void ifElseSats() {
+        // Syntax för if-else-sats:
+        if(uttryck1){
+            // Satser
+        }
+        else{
+            // Satser
+        }
+    }
+
+    private static void ifElseIfElseSats() {
+        // Syntax för if-else-if-else-sats:
+        if(uttryck1){
+            // satser
+        }
+        else if(uttryck2){
+            // satser
+        } // Du kan bygga på med fler else if om du vill.
+        else{
+            // satser
+        }
     }
 
 }
