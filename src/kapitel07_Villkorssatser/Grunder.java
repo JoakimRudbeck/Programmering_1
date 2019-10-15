4917096b526bc80929a0889a294b05e1ac3e6171
sbu470 <sbu470@edu.sollentuna.se>
2019-10-15 15:51:01 +0200

    Update

--- src/kapitel07_Villkorssatser/Grunder.java
+++ src/kapitel07_Villkorssatser/Grunder.java
@@ -1,26 +1,24 @@
 package kapitel07_Villkorssatser;
 
-import java.util.Scanner;
-
 public class Grunder {
 
     private static boolean uttryck1;
     private static boolean uttryck2;
 
     public static void main(String[] args) {
-        ifSats();
-        ifElseSats();
-        ifElseIfElseSats();
+        ifSats_Syntax();
+        ifElseSats_Syntax();
+        ifElseIfElseSats_Syntax();
     }
 
-    private static void ifSats() {
+    private static void ifSats_Syntax() {
         // Syntax för if-sats:
         if(uttryck1){
             // Satser
         }
     }
 
-    private static void ifElseSats() {
+    private static void ifElseSats_Syntax() {
         // Syntax för if-else-sats:
         if(uttryck1){
             // Satser
@@ -30,7 +28,7 @@ public class Grunder {
         }
     }
 
-    private static void ifElseIfElseSats() {
+    private static void ifElseIfElseSats_Syntax() {
         // Syntax för if-else-if-else-sats:
         if(uttryck1){
             // satser
