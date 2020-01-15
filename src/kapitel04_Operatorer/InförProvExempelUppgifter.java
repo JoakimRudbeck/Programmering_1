d30febeedea54d24b24f68038f2ea3914649a356
sbu470 <sbu470@edu.sollentuna.se>
2020-01-15 10:01:44 +0100

    Clean up kap 4

--- src/kapitel04_Operatorer/InförProvExempelUppgifter.java
+++ src/kapitel04_Operatorer/InförProvExempelUppgifter.java
@@ -8,13 +8,7 @@ public class InförProvExempelUppgifter {
         ex3();
         ex4();
         ex5();
-    }
-
-    private static void ex5() {
-        int a = 8;
-        int b = 5 * ++a;
-        int c = (66 % b++) % 11;
-        // Vad är värdet av c?
+        ex6();
     }
 
     private static void ex1() {
@@ -23,6 +17,7 @@ public class InförProvExempelUppgifter {
         minaTal[0] -= minaTal.length % 2;
         // Hur ser minaTal ut efter denna körning?
     }
+
     private static void ex2() {
         int a = 5;
         int b = 5 % 3;
@@ -39,7 +34,6 @@ public class InförProvExempelUppgifter {
         fältet[0] = fältet[2];
         fältet[1] += fältet[0];
     }
-
     private static void ex4() {
         int[] fält = {1,2,3};
         int temp = fält[0];
@@ -47,4 +41,30 @@ public class InförProvExempelUppgifter {
         fält[2] = fält[0];
         // Hur ser fält ut nu?
     }
+
+    private static void ex5() {
+        int a = 8;
+        int b = 5 * ++a;
+        int c = (66 % b++) % 11;
+        // Vad är värdet av c?
+    }
+
+    private static void ex6() {
+        int a = 4;
+        int b = 5;          // a = 4, b = 5
+        b -= (a/2);         // a = 4, b = 3
+        a /= 2;             // a = 2, b = 3
+        a += b;             // a = 5, b = 3
+        a++;                // a = 6, b = 3
+        System.out.println(++a); // skriver ut 7    // a = 7, b = 3
+        b--;                // a = 7, b = 2
+        a *= ++b;           // a = 21, b = 3
+        System.out.println(a); // 21
+        System.out.println(b); // 3
+        System.out.println((a+b) % 5); // 24 % 5 = 4, skriver ut 4
+        // Vad skrivs ut ?
+
+    }
+
+
 }
--- src/kapitel04_Operatorer/Operatorer.java
+++ src/kapitel04_Operatorer/Operatorer.java
@@ -6,55 +6,13 @@ public class Operatorer {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        // Övning 4.2: Göra månader till år och månader:
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Skriv in antal månader, tack:");
-        int månader = Integer.valueOf(scan.nextLine());
-        // exempel: 38 månader = 3*12 + 2 månader
-        // skall ge 3 år och 2 månader.
-        int rest = månader % 12; // 2
-        månader -= rest; // 36
-        int år = månader / 12;
-        System.out.println(år + " år och " + månader + " månader.");
+        tilldelandeOperatorerSid52();
+        moduloExempel();
+        ökaMedEttExempel();
+        tilldelandeOperator();
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // version 1:
-        String[] s1 = new String[3];
-        s1[0] = "hej";
-        s1[1] = "på";
-        s1[2] = "dig";
-
-        // version2:
-        String[] s2 = {"hej", "på", "dig"};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    private static void tilldelandeOperatorerSid52() {
         // Tilldelande operator, sid 52.
 
         int a = 5;
@@ -66,8 +24,6 @@ public class Operatorer {
 
         // v2:
         a += 2;
-
-
 
         int p = 10;
 
@@ -91,58 +47,6 @@ public class Operatorer {
         System.out.println(programmering++);
         System.out.println(++programmering);
         System.out.println(programmering);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //moduloExempel();
-        //tilldelandeOperator();
-        //ökaMedEttExempel();
     }
 
     static void moduloExempel() {
@@ -170,10 +74,11 @@ public class Operatorer {
         System.out.println(b);
         System.out.println(b++); // post-fix, ökar efteråt.
 
-        // 4 sätt att öka en variabels värde med 1:
         int a = 5;
+        // 5 sätt att öka en variabels värde med 1:
         a = a + 1;
         a += 1;
+        a -= -1;
         a++;  // Postfix
         ++a;  // Prefix
     }
