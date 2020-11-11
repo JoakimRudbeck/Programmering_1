7ae4c6dedca3c1d3d2465a634c55ee0c9988d9ce
sbu470 <sbu470@edu.sollentuna.se>
2020-11-11 10:03:53 +0100

    Uppdatera inmatning

--- src/kapitel02_Variabler/Inmatning.java
+++ src/kapitel02_Variabler/Inmatning.java
@@ -3,32 +3,27 @@ import java.util.Scanner;  // Denna rad krävs då vi ska använda klassen Scann
 
 public class Inmatning {
 
+    // Denna variabel är static vilket innebär att den kan användas från hela klassen
+    static Scanner scan = new Scanner(System.in); // För att i Java få input från användaren kan ett Scanner-objekt användas
+
     public static void main(String[] args) {
-        scanner();
-        merScanner();
+        inmatningAvSträng();
+        inmatningAvHeltal();
     }
 
 
-    static void scanner() {
-        Scanner minSkanner = new Scanner(System.in); // För att i Java få input från användaren kan ett Scanner-objekt användas
+    static void inmatningAvSträng() {
         System.out.println("Skriv in ett ord, tack: ");
         // Nu vill vi använda vårt scanner-objekt och anropa dess nextLine-metod.
-        String indata = minSkanner.nextLine();  // nextLine kommer returnera den sträng som användaren skriver in via tangentbordet följt av enter.
+        String indata = scan.nextLine();  // nextLine kommer returnera den sträng som användaren skriver in via tangentbordet följt av enter.
         // variabeln indata innehåller nu den sträng som användren skrev in.
         System.out.println("Du skrev " + indata);
-        System.out.println("Skriv in ett heltal, tack: ");
-        int tal = minSkanner.nextInt(); // Vi kan även använda metoden nextIn om vi vet att användaren kommer skriva in ett heltal.
-        // Notera att om användaren inte skriver in ett heltal då nextInt används får vi ett exekveringsfel och programmet kraschar.
-        System.out.println("Du skrev talet " + tal);
     }
 
-    static void merScanner() {
-        System.out.println("Vad heter du?");
-        Scanner minSkanner = new Scanner(System.in);            // Skapar ett Scanner-objekt
-        String namn = minSkanner.nextLine();                    // Läser in en rad skriven från tangentbordet
+    static void inmatningAvHeltal() {
         System.out.println("Hur gammal är du?");
-        String ålderAsString = minSkanner.nextLine();           // Läser in en till rad från tangentbordet.
-        int ålder = Integer.valueOf(ålderAsString);             // Typomvandling från String till int med hjälpklassen Integer.
-        System.out.println("Du heter " + namn + " och är " + ålder + " år gammal.");
+        String ålderAsString = scan.nextLine();           // Läser in en till rad från tangentbordet.
+        int ålder = Integer.valueOf(ålderAsString);       // Typomvandling från String till int med hjälpklassen Integer.
+        System.out.println("Du är " + ålder + " år gammal.");
     }
 }
