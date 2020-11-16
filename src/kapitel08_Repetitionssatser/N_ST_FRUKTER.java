e576b7077603846620661aac94d4f2e0644f295f
sbu470 <sbu470@edu.sollentuna.se>
2020-11-16 13:47:11 +0100

    Adding frukter

---
+++ src/kapitel08_Repetitionssatser/N_ST_FRUKTER.java
@@ -0,0 +1,38 @@
+package kapitel08_Repetitionssatser;
+
+import java.util.Scanner;
+
+public class N_ST_FRUKTER {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int antalFrukterSomMatcharBokstav = 0; // Kollar hur många frukter som har bokstaven i fråga
+
+        System.out.println("Hur många frukter?");
+        int N = Integer.valueOf(scanner.nextLine());
+
+        String frukter[] = new String[N];  // Skapar ett fält med erfoderligt antal frukter
+
+        // För varje frukt:
+        for (int i = 0; i < frukter.length; i++){  // i = 0,1,2,3,4,5.......N-1
+            System.out.println("Vad är frukt " + (i+1) + "?");
+            frukter[i] = scanner.nextLine();
+        }
+
+        System.out.println("Vilken bokstav?");
+        String bokstav = scanner.nextLine().toLowerCase();
+
+        // kollar varje frukt om den innehåller bokstaven
+        for (int i = 0; i < frukter.length; i++){
+            if (frukter[i].toLowerCase().contains(bokstav)){ // Finns bokstaven frukten ?
+                antalFrukterSomMatcharBokstav++;
+            }
+        }
+
+        System.out.println(antalFrukterSomMatcharBokstav + " av alla frukterna har bokstaven '" + bokstav + "' i sig");
+
+        System.out.println((antalFrukterSomMatcharBokstav)*100.0/frukter.length + " procent av frukterna har bokstaven i sig.\n");
+
+    }
+}
