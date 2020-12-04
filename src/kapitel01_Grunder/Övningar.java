de1efdf84ee00d1c01d7e9dd15a6c64c7fbaacfa
sbu470 <sbu470@edu.sollentuna.se>
2020-12-04 09:15:31 +0100

    update

---
+++ src/kapitel01_Grunder/Övningar.java
@@ -0,0 +1,19 @@
+package kapitel01_Grunder;
+
+public class Övningar {
+
+
+    public static void main(String[] args) {
+        övning1_2();
+    }
+
+    static void övning1_2() {
+        System.out.println("Programmeringsspråk:");
+        System.out.println();
+        System.out.println("C++");
+        System.out.println("Java");
+        System.out.println("Visual Basic");
+    }
+
+
+}
